c54ddc91e4f56ff1b67ce71bac43dba3d343b77c
jason <jasondonovan@microsoft.com>
2024-03-26 17:11:22 +0000

    working version 1.0

--- SimpleBlazorApp/Pages/CalcServiceCompound.cs
+++ SimpleBlazorApp/Pages/CalcServiceCompound.cs
@@ -1,11 +1,16 @@
-﻿namespace SimpleBlazorApp.Pages
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SimpleBlazorApp.Pages
 {
+
+	
 	public class CalcServiceCompund
 	{
-		public decimal CalculateSomething(decimal value, decimal periodic, int apr, int year)
+		public (decimal Amount, decimal Profit) CalculateSomething(decimal value, decimal periodic, int apr, int year)
 		{
 			decimal amount = value; // Start with the initial principal
 			decimal aprPer = apr / 100m;
+		    
 					
 			for (int i = 0; i < year; i++)
 			{
@@ -16,8 +21,9 @@
 				// No need to adjust periodic here; if it's <= 0, it simply won't add to the amount
 				amount += periodic;
 			}
-
-			return amount;
+			decimal profit = amount - value - (periodic*year);
+			
+			return (amount, profit); // Return both amount and profit as a tuple
 		}
 
 	}
